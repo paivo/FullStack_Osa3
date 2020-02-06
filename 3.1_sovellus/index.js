a76adb556b0788312db66acd4a7f8b6e42cc0524
paivo <paveee777@gmail.com>
2020-02-06 18:37:29 +0200

    aloitettiin 3.9

--- 3.1_sovellus/index.js
+++ 3.1_sovellus/index.js
@@ -4,6 +4,9 @@ var morgan = require('morgan')
 const app = express()
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+const cors = require('cors')
+
+app.use(cors())
 
 let persons = [
     { 
@@ -89,8 +92,7 @@ const generateId = () => {
     res.json(person)
   })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
